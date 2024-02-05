e078a134dfc379f0abeabbcb7a82b3f25ff7e0a1
Baby Gupta <Baby.Gupta@teknopoint.local>
2024-02-05 13:34:01 +0530

    up practice setup in react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="recipe-container">
+      <div className="imageBox">
+      <img src="/assets/images/image-omelette.jpeg" alt="omelette image" />
+      </div>
+      <div className="recipe-description">
+          <h1>Simple Omellete recipe</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
