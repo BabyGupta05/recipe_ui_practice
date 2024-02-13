e118d2b77b48b5777687adc505bb0d921592732f
Baby Gupta <Baby.Gupta@teknopoint.local>
2024-02-13 23:10:00 +0530

    css change

--- src/App.js
+++ src/App.js
@@ -35,18 +35,18 @@ function App() {
           </ul>
         </div>
         <hr />
-        <div className="recipe-instruction">
-          <h3>Instructions </h3>
+        <div className="recipe_instruction">
+          <h3 className="recipe_instruction_title">Instructions </h3>
           <ol>
             <li> <span>Beat the eggs</span>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
               You can add a tablespoon of water or milk for a fluffier texture.</li>
 
             <li><span>Heat the pan</span>: Place a non-stick frying pan over medium heat and add butter or oil.</li>
 
-            <li><span></span>Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
+            <li><span>Cook the omelette</span>: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
               the eggs evenly coat the surface.</li>
 
-            <li><span></span>Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the
+            <li><span>Add fillings (optional)</span>: When the eggs begin to set at the edges but are still slightly runny in the
               middle, sprinkle your chosen fillings over one half of the omelette.</li>
 
             <li><span>Fold and serve</span>: As the omelette continues to cook, carefully lift one edge and fold it over the
